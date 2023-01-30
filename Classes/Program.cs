d08cf2a50d5ab83fb380dad5ec54bfab3cf947be
danielocampo226 <sierradanny562@gmail.com>
2023-01-29 19:22:29 -0500

    classes

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var Ford = new Car();
+            Ford.Make = "Ford";
+            Ford.Model = "Focus";
+            Ford.Year = 2013;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX-5",
+                Year = 2016
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2001);
+
+            var carList = new List<Car>() { Ford, mazda, chevy};
+
+            foreach(Car vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}" );
+            }
+            
+            
+           
+
         }
     }
 }
